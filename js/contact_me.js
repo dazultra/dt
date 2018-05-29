699e052818eb798cb32881ff6262895e9315f8b6
Darren Shelley <darren.shelley@gmail.com>
2018-05-29 23:14:32 +0100

    enformed

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/darren@dtpropertyinvestments.co.uk",
+                url: "https://www.enformed.io/pbpi9w7l",
                 type: "POST",
                 data: {
                     name: name,
