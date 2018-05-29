31282c0f2b31a3e077400cc60ea139e71443e538
Darren Shelley <darren.shelley@gmail.com>
2018-05-29 22:59:38 +0100

    update email address

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://formspree.io/darren@dtpropertyinvestments.co.uk",
                 type: "POST",
                 data: {
                     name: name,
