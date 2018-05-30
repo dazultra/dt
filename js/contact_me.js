06b3796c125cca90514b65d15cc8cafc2e1500a9
Darren Shelley <darren.shelley@gmail.com>
2018-05-30 06:24:45 +0100

    back to formspree

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//www.enformed.io/pbpi9w7l",
+                url: "//formspree.io/darren@dtpropertyinvestments.co.uk",
                 type: "POST",
                 data: {
                     name: name,
